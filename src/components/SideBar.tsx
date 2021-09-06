23c6f8dcdaffaf2a59378ca3811be90c39ce9553
WagnerManko <wagner.manko@gmail.com>
2021-09-05 22:05:09 -0300

    Conclusão Desafio 02

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,45 @@
-export function SideBar() {
+import { Button } from '../components/Button';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+
+interface SideBarProps {
+  states: {
+    genres: GenreResponseProps[];
+    selectedGenreId: Number;
+  };
+
+  handleClickButton: Function;
+}
+
+export function SideBar(props: SideBarProps) {
   // Complete aqui
+
+  // function handleClickButton(id: number) {
+  //   // setSelectedGenreId(id);
+  //   alert(id);
+  // }
+
+  return (
+      <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {props.states.genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => props.handleClickButton(genre.id)}
+              selected={props.states.selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
